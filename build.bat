48d4ed74f9a70e61a32d64efce61c4374f46e578
Adam TG <adam.gawelczyk@argosmultilingual.com>
2021-06-04 10:55:24 +0200

    adding publish profile but it does not work

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 
-msbuild  /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" -m 
-REM msbuild  /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" /P:PublishProfile="test" -m 
+REM msbuild  /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" -m 
+msbuild  /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" /P:PublishProfile="publish" -m 
