af37d275011d8760fcdc78544056349afc4b33ef
Adam TG <adam.gawelczyk@argosmultilingual.com>
2021-06-04 11:17:11 +0200

    transofmration prod applied but we have to build csproj so configuration is recognized

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 
 REM msbuild  /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" -m 
 REM msbuild  /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" /P:PublishProfile="publish" -m 
-msbuild ".\ConsoleApp-slow\ConsoleApp-slow.csproj" /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" /P:PublishProfile="publish" -m 
+REM msbuild ".\ConsoleApp-slow\ConsoleApp-slow.csproj" /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" /P:PublishProfile="publish" -m 
+
+msbuild ".\ConsoleApp-slow\ConsoleApp-slow.csproj" /nr:false /t:Rebuild /p:Configuration=prod /p:Platform="Any CPU" -m 
