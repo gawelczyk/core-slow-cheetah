e0adb9f018ebc3a7bf465300b1e7575a04db1cac
Adam TG <adam.gawelczyk@argosmultilingual.com>
2021-06-04 11:11:03 +0200

    calling just project doesn't work, transformation is not applied, it seems it works only for web apps

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 
 REM msbuild  /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" -m 
-msbuild  /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" /P:PublishProfile="publish" -m 
+REM msbuild  /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" /P:PublishProfile="publish" -m 
+msbuild ".\ConsoleApp-slow\ConsoleApp-slow.csproj" /nr:false /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" /P:PublishProfile="publish" -m 
